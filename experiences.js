d44cf3fe04927d3c4b928bc5711e805f7059bc85
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-06-26 15:57:57 -0400

    Added js to experiences page replaced all the lorem ipsum

---
+++ experiences.js
@@ -0,0 +1,40 @@
+// CARD DIMMER
+$(".fishing").mouseenter(function() {
+  $(".fishing").addClass("showFishing");
+});
+
+$(".fishing").mouseleave(function() {
+  $(".fishing").removeClass("showFishing");
+});
+
+$(".painting").mouseenter(function() {
+  $(".painting").addClass("showPainting");
+});
+
+$(".painting").mouseleave(function() {
+  $(".painting").removeClass("showPainting");
+});
+
+$(".bowling").mouseenter(function() {
+  $(".bowling").addClass("showBowling");
+});
+
+$(".bowling").mouseleave(function() {
+  $(".bowling").removeClass("showBowling");
+});
+
+$(".swimming").mouseenter(function() {
+  $(".swimming").addClass("showSwimming");
+});
+
+$(".swimming").mouseleave(function() {
+  $(".swimming").removeClass("showSwimming");
+});
+
+$(".knitting").mouseenter(function() {
+  $(".knitting").addClass("showKnitting");
+});
+
+$(".knitting").mouseleave(function() {
+  $(".knitting").removeClass("showKnitting");
+});
